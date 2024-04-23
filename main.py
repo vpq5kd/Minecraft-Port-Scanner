2e39fd2ddc6a7d1097c34e24fe42ddef47d0654e
vpq5kd <vpq5kd@virginia.edu>
2024-04-23 10:33:27 -0400

    opened repo, configured imports

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+import socket
+import pyfiglet
+
+#resources:
+"""Port-scan basics: https://www.geeksforgeeks.org/port-scanner-using-python/ """
+
+banner = pyfiglet.figlet_format("Minecraft Port Scanner")
+print(banner)
