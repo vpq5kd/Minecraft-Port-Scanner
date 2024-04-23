e8a86e7606db3176611f1d0c3bcc3c247d50a032
vpq5kd <vpq5kd@virginia.edu>
2024-04-23 11:47:39 -0400

    fixed and cleaned the logic, added functionality in the class to parse for any country based on country code

--- IPParser.py
+++ IPParser.py
@@ -4,19 +4,28 @@ import csv
 
 class IPFileParse:
     def __init__(self):
-        self.filePath = "C:\Users\Smspaner\PycharmProjects\MCPortScan\IP2LOCATION-LITE-DB1.CSV"
-    def _read_file(self):
+        self.filePath = "C:\\Users\\Smspaner\\PycharmProjects\\MCPortScan\\IP2LOCATION-LITE-DB1.CSV"
+        self.outFilePath = "parsedIP.CSV"
+    def _read_file(self,country_code):
         ip_array = []
         with open(self.filePath, 'r') as inputFile:
             reader = csv.reader(inputFile)
             for row in reader:
-                if row[2] == "US":
+                if row[2] == country_code:
                     ip_array.append(row)
-        with open("parsedIP.CSV", 'a') as outPutFile:
+        self._clean_file()
+        with open(self.outFilePath, 'a+') as outPutFile:
             for each in ip_array:
-                outPutFile.write(each)
-    def parse(self):
-        self._read_file()
-        return
+                outPutFile.write(','.join(each) + '\n')
+    def _clean_file(self):
+        with open(self.outFilePath, "w") as file:
+            pass
+    def parse(self, country_code):
+        country_code.upper()
+        self._read_file(country_code)
+def main():
+    parser = IPFileParse()
+    parser.parse("US")
+main()
 
 
