0759798e2d70ed7e0f7542510cef647601f5765d
vpq5kd <vpq5kd@virginia.edu>
2024-04-23 11:11:25 -0400

    added ip file for processing

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pyfiglet
 
 #resources:
 """Port-scan basics: https://www.geeksforgeeks.org/port-scanner-using-python/ """
-
+#credits:
+"""MCPortScan uses the IP2Location LITE database for <a href="https://lite.ip2location.com">IP geolocation</a>."""
+#token:
+"""a2ac8205d5bae3"""
 banner = pyfiglet.figlet_format("Minecraft Port Scanner")
 print(banner)
